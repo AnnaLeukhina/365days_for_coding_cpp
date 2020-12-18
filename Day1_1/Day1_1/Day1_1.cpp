501cd7e8e28b3616420181dcd4117c1d58a5eae3
Анна Леухина <helena.good@inbox.ru>
2020-12-19 00:31:00 +0700

    от 18.12.20

---
+++ Day1_1/Day1_1/Day1_1.cpp
@@ -0,0 +1,35 @@
+﻿//18.12.20
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//что такое класс
+//что такое объект класса
+
+class human //human - имя класса
+{//тело класса
+public: //модификатор
+	string name; //age и name - свойства класса human 
+	int age; //свойства класса еть поля класса
+};
+
+class point
+{
+	int x;
+	int y;
+};
+
+int main()
+{
+	human person1; //person1 - объект класса human
+
+	person1.name = "Anna"; //вызов поля name класса human для объекта person1
+	person1.age = 20;
+
+	point a;
+
+	a.x = 0;
+	a.y = 1;
+
+}
---
+++ Test_3/Test_3.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+s
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+
+}
