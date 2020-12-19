46ebcfb078e524aaa640177e4030f46841768913
Анна Леухина <helena.good@inbox.ru>
2020-12-19 15:32:43 +0700

    от 19.12.20

--- Test_3/Test_3.cpp
+++ Test_3/Test_3.cpp
@@ -1,9 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
-s
+
+//const тип_данных ИМЯ_КОНСТАТНТЫ = значение_константы;
+//const тип_данных ИМЯ_КОНСТАТНТЫ_1 = значение_константы_1, ИМЯ_КОНСТАТНТЫ_2 = значение_константы_2, ИМЯ_КОНСТАТНТЫ_...;
 
 void main()
 {
     setlocale(LC_ALL, "rus");
+
+    const int DAYS_IN_WEEK = 7;
+    cout << DAYS_IN_WEEK << endl;
+    
+    const char NEW_LINE = '\n';
+    const char TAB = '\t';
+    cout << TAB << DAYS_IN_WEEK << NEW_LINE;
+
 
 }
---
+++ Test_6/Test_6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "ЗАДАНИЕ:\nВведите три числа и выведите на экран значение суммы,\nпроизведение и среднее арифметическое этих чисел.\n\n";
+    float a, b, c;
+    cout << "Введите три числа: ";
+    cin >> a >> b >> c;
+    cout << "Сумма: " << a + b + c << endl;
+    cout << "Произведение: " << a * b * c << endl;
+    cout << "Среднее арифметическое: " << (a + b + c)/3 << endl;
+}
