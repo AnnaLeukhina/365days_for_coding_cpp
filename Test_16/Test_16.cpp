f952d5c0c55af6c127598585812599987a9d0eaa
Анна Леухина <helena.good@inbox.ru>
2020-12-22 20:34:53 +0700

    от 21.12.20

---
+++ Test_16/Test_16.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+//Написать программу, которая находит сумму всех целых нечётных чисел в диапазоне, указанном пользователем
+//do while
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int rangeBegin, rangeEnd, sum = 0;
+
+    cout << "Введите начало и конец диапазона: ";
+    cin >> rangeBegin >> rangeEnd;
+
+    if ((rangeBegin == rangeEnd && rangeBegin%2 == 0) || rangeBegin > rangeEnd) //проверка на правильный ввод диапазона
+    {
+        cout << "Некорректный ввод диапазона";
+        return 0;
+    }    
+
+    do
+    {
+        if (rangeBegin % 2 == 1)
+            sum += rangeBegin;
+        rangeBegin++;
+    } while (rangeBegin <= rangeEnd);
+
+    cout << "\nСумма нечётных чисел диапазона: " << sum;
+}
---
+++ Test_17/Test_17.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+//Цикл for
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "for (int i = 0; i < 5; i++)" << endl;
+    //for (инициализация итератора; условие выполнения цикла; изменение итератора)
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << "Переменная i = " << i << endl;
+    }
+    cout << endl << endl;
+
+    cout << "for (a = 0; a < 5; a++)" << endl;
+    int a; //объявление оператора до цикла
+    for (a = 0; a < 5; a++)
+    {
+        cout << "Переменная a = " << a << endl;
+    }
+    cout << endl << endl;
+
+    cout << "for (; b < 5; b++)" << endl;
+    int b = 0; //инициализация оператора до цикла
+    for (; b < 5; b++)
+    {
+        cout << "Переменная b = " << b << endl;
+    }
+    cout << endl << endl;
+
+    cout << "for (; b < 10; b++)" << endl;
+    //цикл использует значение итератора после выполнения цикла выше
+    for (; b < 10; b++) 
+    {
+        cout << "Переменная b = " << b << endl;
+    }
+    cout << endl << endl;
+
+    cout << "for (int c = 0; c < 5; )" << endl; 
+    //изменение итератора внутри цикла
+    for (int c = 0; c < 5; )
+    {
+        cout << "Переменная c = " << c << endl;
+        c++;
+    }
+    cout << endl << endl;
+
+    cout << "for (int с = 0; с < 5; )" << endl;
+    //изменение итератора внутри цикла
+    for (int с = 0; с < 5; )
+    {
+        с++;
+        cout << "Переменная с = " << с << endl;
+    }
+    cout << endl << endl;
+
+    //cout << "for (; ;)" << endl;
+    ////бесконечный цикл
+    //for (; ;)
+    //{
+    //    cout << "Бесконечный цикл" << endl;
+    //}
+}
